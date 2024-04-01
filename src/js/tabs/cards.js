89d685c64b4f251fb58559a7ec5ec005017e0a07
ivankprod <houseprotector@mail.ru>
2024-04-02 01:03:26 +0500

    feat(page): tabs completed

---
+++ src/js/tabs/cards.js
@@ -0,0 +1,331 @@
+import imageStarRating from "../../assets/svg/star_rating.svg";
+
+import imageSellers1 from "../../assets/img/sellers/1.png";
+import imageSellers2 from "../../assets/img/sellers/2.png";
+import imageSellers3 from "../../assets/img/sellers/3.png";
+
+import imageTrending1 from "../../assets/img/trending/1.png";
+import imageTrending2 from "../../assets/img/trending/2.png";
+import imageTrending3 from "../../assets/img/trending/3.png";
+
+export const cardsSellers = `
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_mint-cream">
+		<img
+			class="card__photo-img"
+			src="${imageSellers1}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat Rockerz 333
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$20</div>
+		</div>
+		<div class="card__discount card__discount_color_primary">
+			Save 60<br />%
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_light-cyan">
+		<img
+			class="card__photo-img"
+			src="${imageSellers2}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat kerz 234
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$40</div>
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_light-mallow">
+		<img
+			class="card__photo-img"
+			src="${imageSellers3}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat Rockerz 323
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$30</div>
+		</div>
+		<div class="card__discount card__discount_color_primary">
+			Save 40<br />%
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>`;
+
+export const cardsTrending = `
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_mint-cream">
+		<img
+			class="card__photo-img"
+			src="${imageTrending1}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat Rockerz 333
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$20</div>
+		</div>
+		<div class="card__discount card__discount_color_primary">
+			Save 60<br />%
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_light-cyan">
+		<img
+			class="card__photo-img"
+			src="${imageTrending2}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat kerz 234
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$40</div>
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>
+<div class="card-wrapper animate-fadein-long-css">
+	<div class="card card_color_light-mallow">
+		<img
+			class="card__photo-img"
+			src="${imageTrending3}"
+			height="200"
+			alt="earphones1"
+		/>
+		<div class="card__info">
+			<div class="card__info-title">
+				Boat Rockerz 323
+			</div>
+			<div class="card__info-rating">
+				<div class="rating__stars">
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+					<img
+						src="${imageStarRating}"
+						draggable="false"
+						alt="star"
+					/>
+				</div>
+				<div class="rating__reviews">
+					75 Reviews
+				</div>
+			</div>
+			<div class="card__info-price">$30</div>
+		</div>
+		<div class="card__discount card__discount_color_primary">
+			Save 40<br />%
+		</div>
+	</div>
+	<button class="button button_color_green card-btn">
+		Add to cart
+	</button>
+</div>`;
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./assets/scss/styles.scss"
-
-document.querySelector(".mobile-nav-btn_open")?.addEventListener("click", function() {
-	document.querySelector(".nav_mobile").style.display = "flex";
-});
-
-document.querySelector(".mobile-nav-btn_close")?.addEventListener("click", function() {
-	document.querySelector(".nav_mobile").style.display = "none";
-});
